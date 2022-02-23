75ea138fe516f071867f0f6ab5bb84a0f235b871
rasipriyanka <priyankapendela111@gmail.com>
2022-02-23 13:56:34 +0530

    [priyanka]Added the implementation For EmpWageUC3

--- EmpWage.js
+++ EmpWage.js
@@ -1,23 +1,26 @@
-//--------UC2 Using switch stmt daily emp  wage----------//
+//----uc3-----//
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const EMP_RATE_PER_HR = 20;
 let empHrs = 0;
-//----To Check Whether the employee is full time part time or work based---//
-switch (empInput) {
-    case IS_FULL_TIME:
-        empHrs = 8;
-        console.log("FullTime Employee")
-        break;
-    case IS_PART_TIME:
-        empHrs = 8;
-        console.log("PartTime Employee")
-        break;
-    default:
-        empHrs = 0;
-        console.log("absent")
-        break;
+
+function GetWorkingHrs(empInput) {
+    switch (empInput) {
+        case IS_FULL_TIME:
+            empHrs = 8;
+            console.log("FullTime Employee")
+            return empHrs;
+        case IS_PART_TIME:
+            empHrs = 8;
+            console.log("PartTime Employee")
+            return empHrs;
+        default:
+            empHrs = 0;
+            console.log("absent")
+            return empHrs;
+    }
 }
+empHrs = GetWorkingHrs(empInput);
 //-----calculate daily wage-------//
 let dailWage = EMP_RATE_PER_HR * empHrs;
-console.log("UC2 DAily wage:" + dailWage);
+console.log("UC3 DAily wage:" + dailWage);
