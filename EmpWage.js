b33c053fafd8da18862a2725a479aba476e8e293
rasipriyanka <priyankapendela111@gmail.com>
2022-02-24 12:04:55 +0530

    [Priyanka]Added the implementation for usecase5

--- EmpWage.js
+++ EmpWage.js
@@ -1,4 +1,4 @@
-//----uc4-----//
+//----uc5-----//
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const EMP_RATE_PER_HR = 20;
@@ -21,13 +21,15 @@ function GetWorkingHrs(empInput) {
     }
 }
 const MAX_WORKING_DAYS = 20;
-let totalWage = 0;
-for (let day = 1; day <= MAX_WORKING_DAY; day++) {
+const MAX_WORKING_HRS=80;
+let totalWage = 0,day=1;
+while(day <= MAX_WORKING_DAY && hrs<= MAX_WORKING_HRS) {
     let randomInput = Math.floor(Math.random() * 10) % 3;
     empHrs = GetWorkingHrs(randomInput);
-
     let dailWage = EMP_RATE_PER_HR * empHrs;
-    console.log("UC3 DAily wage:" + dailWage);
+    console.log("UC5 day"+day+"wage:"+dailywage);
     totalWage += dailyWage;
+    day++;
+    hrs+=empHrs;
 }
-console.log("UC4 TotalWage for 20 days:" + totalWage);
+console.log("UC5 TotalWage for "+MAX_WORKING_DAYS+"days:"+hrs+"hrs"+totalWage);
