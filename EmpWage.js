496ee48ab5beae1b66a8588d125868be1e3d8e36
rasipriyanka <priyankapendela111@gmail.com>
2022-03-02 17:24:11 +0530

    [Priyanka]Added the implementation for regex

--- EmpWage.js
+++ EmpWage.js
@@ -15,7 +15,13 @@ class EmployeePayrollData{
     }
     //getter and setter method
     get name () {return this._name;}
-    set name(name) {this._name=name;}
+    set name(name) 
+    {
+        let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if(nameRegex.test(name))
+        this._name=name;
+        else throw 'Name is Incorrect!';
+    }
 
     //method
     toString(){
@@ -27,7 +33,11 @@ return "id=" + this.id + ",name='" +this.name +",salary="+this.salary+" ,"+"gend
 }
 let employeePayrollData=new EmployeePayrollData(1,"Mark",30000)
 console.log(employeePayrollData.toString())
+try{
 employeePayrollData.name="john";
 console.log(employeePayrollData.toString());
+}catch(e){
+    console.error(e);
+}
 let newEmployeePayrollData = new EmployeePayrollData(1,"Terrie",3000,"F",newData());
 console.log(newEmployeePayrollData.toString());
