632532d366d7f5df3b43d614e88f1d4f329fd489
rasipriyanka <priyankapendela111@gmail.com>
2022-03-02 16:12:48 +0530

    [Priyanka]Added earned in asingle day for the employee daily wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,9 +1,9 @@
-//-----UC8 Use Map To store Emp Daily Wage--------//
+//-----UC10--------//
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
-const Working_Per_Hr=20;
-let totalEmployeeWage=0;
-let empHr=0;
+const Working_Per_Hr = 20;
+let totalEmployeeWage = 0;
+let empHr = 0;
 let empHrs = 0;
 
 function GetWorkingHrs(empInput) {
@@ -22,37 +22,27 @@ function GetWorkingHrs(empInput) {
             return empHrs;
     }
 }
-function calculateDailyWage(empHr)
-{
-    return empHr*Working_Per_Hr;
+function calculateDailyWage(empHr) {
+    return empHr * Working_Per_Hr;
 }
-    const MAX_HRS_IN_MONTH = 160;
-    const NUM_OF_WORKING_DAYS = 20;
-    let totalEmpHrs = 0;
-    let totalWorkingDays = 0;
-    let empDailyWageArr = new Array();
-    let empDailyWageMap = new Map();
-  
-    function calcDailyWage(empHrs) {
-      return empHrs * Working_Per_Hr;
-    }
-  
-    while (
-      totalEmpHrs <= MAX_HRS_IN_MONTH &&
-      totalWorkingDays < NUM_OF_WORKING_DAYS
-    ) {
-      totalWorkingDays++;
-      let empCheck = Math.floor(Math.random() * 10) % 3;
-      let empHrs = GetWorkingHrs(empCheck);
-      totalEmpHrs += empHrs;
-      empDailyWageArr.push(calcDailyWage(empHrs));
-      empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
-    }
-    console.log(empDailyWageMap);
-    function totalWages(totalWage, dailyWage) {
-      return totalWage + dailyWage;
-    }
-    console.log(
-      "UC8 - Total Emp Wage: " +
-        Array.from(empDailyWageMap.values()).reduce(totalWages, 0)
-    );
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyHrsAndWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = GetWorkingHrs(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsAndWageArr.push({
+        dayNum: totalWorkingDays,
+        dailyHours: empHrs,
+        dailyWage: calculateDailyWage(empHrs),
+        toString() {
+            console.log("\nDay" + this.dayNum + " => Working Hours is " + this.dailyHours + " And Wage Earned = " + this.dailyWage);
+        },
+    });
+}
+console.log(
+    "\nUC10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr)
